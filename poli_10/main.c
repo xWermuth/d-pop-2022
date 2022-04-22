ebe894819acab3c87e9cc59dc41cb075e4e24a44
alwe <alwe@itu.dk>
2022-04-22 16:31:42 +0200

    Completed poli-10

---
+++ poli_10/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("poli-%i \n", i);
+    }
+    printf("du slap fri\n");
+
+    return 0;
+}
